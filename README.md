7f639ce0576154fb4febd195ec0ce5ebaca5f2cf
mtso <mtso@tuta.io>
2017-10-26 00:59:35 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# ghost
+
+A program to recursively delete the file contents in a file directory  
+but keep the file structure in tact.
---
+++ ghost.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"path"
+	"fmt"
+	"os"
+	"io/ioutil"
+)
+
+const USER_FILEMODE = 0644
+const GHOST_MESSAGE = "ghosted:"
+const BLANK = []byte{}
+
+func ghost(pathname string, file os.FileInfo) {
+	p := path.Join(pathname, file.Name())
+
+	if file.IsDir() {	
+		fs, err := ioutil.ReadDir(p)
+		if err != nil {
+			fmt.Println("error on:", file.Name())
+			return
+		}
+		for _, v := range fs {
+			ghost(p, v)
+		}
+	} else {
+		ioutil.WriteFile(p, BLANK, USER_FILEMODE)
+		fmt.Println(GHOST_MESSAGE, p)
+	}
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Need a filepath")
+		return
+	}
+
+	root := os.Args[1]
+	if fs, err := ioutil.ReadDir(root); err == nil {
+		for _, v := range fs {
+			ghost(root, v)
+		}
+	} else {
+		fmt.Println(err)
+	}
+}
